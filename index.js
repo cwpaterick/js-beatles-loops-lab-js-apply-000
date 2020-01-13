f1f0d471d5764dc90612dcae52b85a0344aac24f
connor paterick <cwpaterick@gmail.com>
2020-01-13 02:37:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,12 +17,3 @@ function johnLennonFacts(facts){
   }
 }
 
-function iLoveTheBeatles(number){
-  var array = [];{
-    do {
-      array.push("I love the Beatles!")
-      number++;
-      }while ( number < 15 );
-  }
-  return array;
-}
