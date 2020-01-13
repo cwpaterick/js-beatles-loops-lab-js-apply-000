20496987652432748133ae0f010fd478242de417
connor paterick <cwpaterick@gmail.com>
2020-01-13 02:45:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function theBeatlesPlay(musicians, instruments){
+  var array = [];{
+    for (var i = 0; i < musicians.length; i++){
+    array.push(musicians[i]+" plays "+ instruments[i]);}
+  }
+  return array;
+}
+
+function johnLennonFacts(facts){
+  var i = 0;
+  var array = [];{
+  while ( i < facts.length){
+  array.push(facts[i]+"!!!");
+  i++;
+  }
+  return array;
+  }
+}
